09e9490fa7f67794d75c02d2463d5d0f92aadc81
Mrityunjay Kumar Dwivedi <86121370+Maaady@users.noreply.github.com>
2024-06-20 13:35:49 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -7,10 +7,10 @@ def multiply(number1, number2):
 def divide(number1, number2): 
     return (number1 / number2)
   
-print("Operation Menu -\n" \ 
-        "1. Addition of two numbers\n" \ 
-        "2. Subtraction of two numbers\n" \ 
-        "3. Multiplication of two numbers\n" \ 
+print("Operation Menu -\n" 
+        "1. Addition of two numbers\n"  
+        "2. Subtraction of two numbers\n" 
+        "3. Multiplication of two numbers\n" 
         "4. Division of two numbers\n") 
 
 select = input("Select operations form 1, 2, 3, 4 :") 
@@ -34,4 +34,4 @@ elif select == '4':
     print(num1, "/", num2, "=", 
                     divide(num1, num2)) 
 else: 
-    print("Invalid input!"
+    print("Invalid input!")
